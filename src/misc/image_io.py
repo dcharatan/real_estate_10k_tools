acd4fcefea3836d736a6b5444e74d8c02dba9c44
David Charatan <david.charatan@gmail.com>
2023-06-29 06:29:33 -0400

    Split save_image into save_image and prep_image.

--- src/misc/image_io.py
+++ src/misc/image_io.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from typing import Union
 
+import numpy as np
 import torch
 from einops import rearrange, repeat
-from jaxtyping import Float
+from jaxtyping import Float, UInt8
 from PIL import Image
 from torch import Tensor
 
@@ -14,12 +15,7 @@ FloatImage = Union[
 ]
 
 
-def save_image(
-    image: FloatImage,
-    path: Union[Path, str],
-) -> None:
-    """Save an image. Assumed to be in range 0-1."""
-
+def prep_image(image: FloatImage) -> UInt8[np.ndarray, "height width channel"]:
     # Handle batched images.
     if image.ndim == 4:
         image = rearrange(image, "b c h w -> c h (b w)")
@@ -34,11 +30,19 @@ def save_image(
         image = repeat(image, "() h w -> c h w", c=3)
     assert image.shape[0] in (3, 4)
 
+    image = (image.detach().clip(min=0, max=1) * 255).type(torch.uint8)
+    return rearrange(image, "c h w -> h w c").cpu().numpy()
+
+
+def save_image(
+    image: FloatImage,
+    path: Union[Path, str],
+) -> None:
+    """Save an image. Assumed to be in range 0-1."""
+
     # Create the parent directory if it doesn't already exist.
     path = Path(path)
     path.parent.mkdir(exist_ok=True, parents=True)
 
     # Save the image.
-    image = (image.detach().clip(min=0, max=1) * 255).type(torch.uint8)
-    image = rearrange(image, "c h w -> h w c")
-    Image.fromarray(image.cpu().numpy()).save(path)
+    Image.fromarray(prep_image(image)).save(path)
