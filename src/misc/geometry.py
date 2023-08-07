1a570579b694ff0e11fa2bc280867abff899af28
David Charatan <david.charatan@gmail.com>
2023-08-07 16:19:40 -0400

    Copy over useful stuff

--- src/misc/geometry.py
+++ src/misc/geometry.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 
 import torch
-from einops import einsum
+from einops import einsum, rearrange, reduce, repeat
 from jaxtyping import Float, Int64
 from torch import Tensor
 
@@ -118,3 +118,86 @@ def sample_image_grid(
     coordinates = torch.stack(torch.meshgrid(x, y, indexing="xy"), dim=-1)
 
     return coordinates, selector
+
+
+def sample_training_rays(
+    image: Float[Tensor, "batch view channel height width"],
+    extrinsics: Float[Tensor, "batch view 4 4"],
+    intrinsics: Float[Tensor, "batch view 3 3"],
+    num_rays: int,
+) -> Tuple[
+    Float[Tensor, "batch ray 3"],  # origins
+    Float[Tensor, "batch ray 3"],  # directions
+    Float[Tensor, "batch ray 3"],  # sampled color
+]:
+    device = extrinsics.device
+    b, v, _, h, w = image.shape
+
+    # Generate all possible target rays.
+    xy, _ = sample_image_grid(h, w, device)
+    xy = repeat(xy, "h w xy -> (b v) (h w) xy", b=b, v=v)
+    origins, directions = get_world_rays(
+        xy,
+        rearrange(extrinsics, "b v i j -> (b v) i j"),
+        rearrange(intrinsics, "b v i j -> (b v) i j"),
+    )
+    origins = rearrange(origins, "(b v) (h w) xy -> b (v h w) xy", b=b, v=v, h=h, w=w)
+    directions = rearrange(
+        directions, "(b v) (h w) xy -> b (v h w) xy", b=b, v=v, h=h, w=w
+    )
+    pixels = rearrange(image, "b v c h w -> b (v h w) c")
+
+    # Sample random rays.
+    num_possible_rays = v * h * w
+    ray_indices = torch.randint(num_possible_rays, (b, num_rays), device=device)
+    batch_indices = repeat(torch.arange(b, device=device), "b -> b n", n=num_rays)
+
+    return (
+        origins[batch_indices, ray_indices],
+        directions[batch_indices, ray_indices],
+        pixels[batch_indices, ray_indices],
+    )
+
+
+def intersect_rays(
+    origins_x: Float[Tensor, "*#batch 3"],
+    directions_x: Float[Tensor, "*#batch 3"],
+    origins_y: Float[Tensor, "*#batch 3"],
+    directions_y: Float[Tensor, "*#batch 3"],
+) -> Float[Tensor, "*batch 3"]:
+    """Compute the least-squares intersection of rays. Uses the math from here:
+    https://math.stackexchange.com/a/1762491/286022
+    """
+
+    # Broadcast the rays so their shapes match.
+    shape = torch.broadcast_shapes(
+        origins_x.shape,
+        directions_x.shape,
+        origins_y.shape,
+        directions_y.shape,
+    )
+    origins_x = origins_x.broadcast_to(shape)
+    directions_x = directions_x.broadcast_to(shape)
+    origins_y = origins_y.broadcast_to(shape)
+    directions_y = directions_y.broadcast_to(shape)
+
+    # Stack the rays into (2, *shape).
+    origins = torch.stack([origins_x, origins_y], dim=0)
+    directions = torch.stack([directions_x, directions_y], dim=0)
+    dtype = origins.dtype
+    device = origins.device
+
+    # Compute n_i * n_i^T - eye(3) from the equation.
+    *batch, _ = shape
+    n = einsum(directions, directions, "r ... i, r ... j -> r ... i j")
+    n = n - torch.eye(3, dtype=dtype, device=device).broadcast_to((2, *batch, 3, 3))
+
+    # Compute the left-hand side of the equation.
+    lhs = reduce(n, "r ... i j -> ... i j", "sum")
+
+    # Compute the right-hand side of the equation.
+    rhs = einsum(n, origins, "r ... i j, r ... j -> r ... i")
+    rhs = reduce(rhs, "r ... i -> ... i", "sum")
+
+    # Left-matrix-multiply both sides by the pseudo-inverse of lhs to find p.
+    return torch.linalg.lstsq(lhs, rhs).solution
