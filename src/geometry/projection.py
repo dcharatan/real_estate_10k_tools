9f0eed096b7f92d4f7615d8bb9838411bd56e728
David Charatan <david.charatan@gmail.com>
2023-08-18 01:34:05 -0400

    Make projection code work in both 2D and 3D.

--- src/geometry/projection.py
+++ src/geometry/projection.py
@@ -7,82 +7,82 @@ from torch import Tensor
 
 
 def homogenize_points(
-    points: Float[Tensor, "*batch n"],
-) -> Float[Tensor, "*batch n+1"]:
+    points: Float[Tensor, "*batch dim"],
+) -> Float[Tensor, "*batch dim+1"]:
     """Convert batched points (xyz) to (xyz1)."""
     return torch.cat([points, torch.ones_like(points[..., :1])], dim=-1)
 
 
 def homogenize_vectors(
-    vectors: Float[Tensor, "*batch n"],
-) -> Float[Tensor, "*batch n+1"]:
+    vectors: Float[Tensor, "*batch dim"],
+) -> Float[Tensor, "*batch dim+1"]:
     """Convert batched vectors (xyz) to (xyz0)."""
     return torch.cat([vectors, torch.zeros_like(vectors[..., :1])], dim=-1)
 
 
 def transform_rigid(
-    homogeneous_xyz: Float[Tensor, "*#batch 4"],
-    transformation: Float[Tensor, "*#batch 4 4"],
-) -> torch.Tensor:
+    homogeneous_coordinates: Float[Tensor, "*#batch dim"],
+    transformation: Float[Tensor, "*#batch dim dim"],
+) -> Float[Tensor, "*batch dim"]:
     """Apply a rigid-body transformation to points or vectors."""
-    return einsum(transformation, homogeneous_xyz, "... i j, ... j -> ... i")
+    return einsum(transformation, homogeneous_coordinates, "... i j, ... j -> ... i")
 
 
 def transform_cam2world(
-    homogeneous_camera_xyz: Float[Tensor, "*#batch 4"],
-    cam2world: Float[Tensor, "*#batch 4 4"],
-) -> Float[Tensor, "*batch 4"]:
+    homogeneous_coordinates: Float[Tensor, "*#batch dim"],
+    extrinsics: Float[Tensor, "*#batch dim dim"],
+) -> Float[Tensor, "*batch dim"]:
     """Transform points from 3D camera coordinates to 3D world coordinates."""
-    return transform_rigid(homogeneous_camera_xyz, cam2world)
+    return transform_rigid(homogeneous_coordinates, extrinsics)
 
 
 def transform_world2cam(
-    homogeneous_camera_xyz: Float[Tensor, "*#batch 4"],
-    cam2world: Float[Tensor, "*#batch 4 4"],
-) -> Float[Tensor, "*batch 4"]:
+    homogeneous_coordinates: Float[Tensor, "*#batch dim"],
+    extrinsics: Float[Tensor, "*#batch dim dim"],
+) -> Float[Tensor, "*batch dim"]:
     """Transform points from 3D world coordinates to 3D camera coordinates."""
-    return transform_rigid(homogeneous_camera_xyz, cam2world.inverse())
+    return transform_rigid(homogeneous_coordinates, extrinsics.inverse())
 
 
 def project(
-    points: Float[Tensor, "*#batch 3"],
-    intrinsics: Float[Tensor, "*#batch 3 3"],
+    points: Float[Tensor, "*#batch dim"],
+    intrinsics: Float[Tensor, "*#batch dim dim"],
     epsilon: float = torch.finfo(torch.float32).eps,
-) -> Float[Tensor, "*batch 2"]:
+) -> Float[Tensor, "*batch dim-1"]:
     points = points / (points[..., -1:] + epsilon)
     points = einsum(intrinsics, points, "... i j, ... j -> ... i")
     return points[..., :2]
 
 
 def unproject(
-    coordinates_xy: Float[Tensor, "*#batch 2"],
+    coordinates: Float[Tensor, "*#batch dim"],
     z: Float[Tensor, "*#batch"],
-    intrinsics: Float[Tensor, "*#batch 3 3"],
-) -> Float[Tensor, "*batch 3"]:
+    intrinsics: Float[Tensor, "*#batch dim+1 dim+1"],
+) -> Float[Tensor, "*batch dim+1"]:
     """Unproject 2D camera coordinates with the given Z values."""
 
     # Apply the inverse intrinsics to the coordinates.
-    coordinates_xy = homogenize_points(coordinates_xy)
-    coordinates_xyz = einsum(
-        intrinsics.inverse(), coordinates_xy, "... i j, ... j -> ... i"
+    coordinates = homogenize_points(coordinates)
+    ray_directions = einsum(
+        intrinsics.inverse(), coordinates, "... i j, ... j -> ... i"
     )
 
     # Apply the supplied depth values.
-    return coordinates_xyz * z[..., None]
+    return ray_directions * z[..., None]
 
 
 def get_world_rays(
-    coordinates_xy: Float[Tensor, "*#batch 2"],
-    extrinsics: Float[Tensor, "*#batch 4 4"],
-    intrinsics: Float[Tensor, "*#batch 3 3"],
+    coordinates: Float[Tensor, "*#batch dim"],
+    extrinsics: Float[Tensor, "*#batch dim+2 dim+2"],
+    intrinsics: Float[Tensor, "*#batch dim+1 dim+1"],
 ) -> Tuple[
-    Float[Tensor, "*batch 3"],  # origins
-    Float[Tensor, "*batch 3"],  # directions
+    Float[Tensor, "*batch dim+1"],  # origins
+    Float[Tensor, "*batch dim+1"],  # directions
 ]:
     # Get camera-space ray directions.
     directions = unproject(
-        coordinates_xy,
-        torch.ones_like(coordinates_xy[..., 0]),
+        coordinates,
+        torch.ones_like(coordinates[..., 0]),
         intrinsics,
     )
     directions = directions / directions.norm(dim=-1, keepdim=True)
@@ -92,7 +92,7 @@ def get_world_rays(
     directions = transform_cam2world(directions, extrinsics)[..., :3]
 
     # Tile the ray origins to have the same shape as the ray directions.
-    origins = extrinsics[..., :3, 3].broadcast_to(directions.shape)
+    origins = extrinsics[..., :-1, -1].broadcast_to(directions.shape)
 
     return origins, directions
 
